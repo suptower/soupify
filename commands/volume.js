e8589d72b6a9f6c1b942ffbd4421489f99182762
Arda K <70473844+suptower@users.noreply.github.com>
2021-12-16 11:25:45 +0100

    volume

---
+++ commands/volume.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('volume')
+		.setDescription('Set volume of bot (has to be between 0 and 100')
+        .addIntegerOption(option => {
+            option.setName('percent')
+                .setRequired(true);
+        }),
+	async execute(interaction, distube) {
+        await interaction.deferReply();
+        const queue = distube.getQueue(interaction.guild);
+        const newVol = interaction.options.getInteger('percent');
+        if (!queue) {
+            return interaction.editReply("This does only work if there is a queue.");
+        }
+        else {
+            if (newVol >= 0 && newVol <= 100) {
+                distube.setVolume(queue,newVol);
+            }
+            else {
+                return interaction.editReply("Your percentage needs to be a number between 0 and 100.");
+            }
+        }
+        return interaction.editReply("⏸️   Player paused.");
+	},
+};
