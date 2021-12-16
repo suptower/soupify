fd81801fce6116f6ea5f6b56b22b6be38ac00e42
Arda K <70473844+suptower@users.noreply.github.com>
2021-12-16 11:15:42 +0100

    jump. remove

---
+++ commands/jump.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('jump')
+		.setDescription('Jump to a song in queue')
+        .addIntegerOption(option =>
+                option.setName('position')
+                    .setDescription('Position to jump to')
+                    .setRequired(true)),
+	async execute(interaction, distube) {
+        await interaction.deferReply();
+        const pos = interaction.options.getInteger('position');
+        const queue = distube.getQueue(interaction.guild);
+        if (queue) {
+            if (!(pos == null)) {
+                if (pos > 0 && pos < queue.songs.length) {
+                    distube.jump(interaction.guild,pos);
+                }
+                else {
+                    return interaction.editReply("Your given position is not in accepted range.");
+                }
+            }
+        }
+        else {
+            return interaction.editReply("There is no queue.");
+        }
+        return interaction.editReply("📼   Jumped to position `"+pos+"` in queue.");
+	},
+};
