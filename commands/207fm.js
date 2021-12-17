8b8a3795e336cdb55b7f7836da52ed1870693ed3
suptower <ardakocer@live.de>
2021-12-17 14:55:25 +0100

    embed update

--- commands/207fm.js
+++ commands/207fm.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('207fm')
@@ -8,6 +9,15 @@ module.exports = {
         if (!interaction.member.voice.channel) {
             return interaction.editReply("You need to be connected to a voice channel.");
         }
+        const InfoEmbed = new MessageEmbed()
+        .setColor('#1db954')
+        .setTitle("APACHE 207")
+        .setThumbnail('https://i.imgur.com/CrdWQZh.jpg')
+        .setDescription('Der Gangster, der ab und an das Tanzbein schwingt.')
+        .addFields(
+            { name: 'Spotify', value: 'https://spoti.fi/3m9BCgg' },
+            { name: 'Playlist', value: 'https://spoti.fi/3e7i3k9' }
+        );
         const vc = interaction.member.voice.channel;
         const songString = "https://open.spotify.com/playlist/7x1xrBl5lVg63ppvEaxdrb?si=8e11c61e50134ad8";
         const queue = distube.getQueue(interaction.guild);
@@ -15,6 +25,7 @@ module.exports = {
             member: interaction.member,
             textChannel: interaction.channel,
         });
+        queue.textChannel.send({embeds: [InfoEmbed]});
         return await interaction.editReply("<:apache207:921394699456102410>   Successfully added `APACHE BLEIBT GLEICH` to the queue.");
 	},
 };
--- commands/essentials.js
+++ commands/essentials.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('essentials')
@@ -8,6 +9,15 @@ module.exports = {
         if (!interaction.member.voice.channel) {
             return interaction.editReply("You need to be connected to a voice channel.");
         }
+        const InfoEmbed = new MessageEmbed()
+        .setColor('#1db954')
+        .setTitle("ESSENTIALS 2010")
+        .setThumbnail('https://i.imgur.com/CrdWQZh.jpg')
+        .setDescription('A playlist by suptower, honoring the best of the past decade.')
+        .addFields(
+            { name: 'Author', value: 'https://spoti.fi/3yz6Wdg' },
+            { name: 'Playlist', value: 'https://spoti.fi/3GUVTOr' }
+        );
         const vc = interaction.member.voice.channel;
         const songString = "https://open.spotify.com/playlist/1FQypuz87kt9ICJi64CjHq?si=2a03feeb2f8c4cd5";
         const queue = distube.getQueue(interaction.guild);
@@ -15,6 +25,7 @@ module.exports = {
             member: interaction.member,
             textChannel: interaction.channel,
         });
+        queue.textChannel.send({embeds: [InfoEmbed]});
         return await interaction.editReply("<:essentials:921394784915050516>   Successfully added `APACHE BLEIBT GLEICH` to the queue.");
 	},
 };
