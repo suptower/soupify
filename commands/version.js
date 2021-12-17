cf3daad6dd6756b3c5154996cb6a358c4864f768
suptower <ardakocer@live.de>
2021-12-17 14:44:13 +0100

    version update

---
+++ commands/version.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('version')
+		.setDescription('Shows bot related information.'),
+	async execute(interaction, distube) {
+        await interaction.deferReply();
+        const InfoEmbed = new MessageEmbed()
+        .setColor('#1db954')
+        .setTitle("Information")
+        .setAuthor('soupify by suptower', 'https://i.imgur.com/eB4PoaU.png')
+        .setThumbnail('https://i.imgur.com/8hL40D0.png')
+        .setDescription('Discord music bot powered by soupBot, provided by suptower.')
+        .addFields(
+            { name: '\u200B', value: '\u200B' },
+            { name: 'Repository', value: 'https://github.com/suptower/soupify' },
+        )
+        .setTimestamp()
+        .setFooter('Version 1.0.3 | Updated: 17th of December, 2021');
+    await interaction.editReply({embeds: [InfoEmbed]});
+	},
+};
