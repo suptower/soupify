526a8f228d257a502b66fab430d65e5e53abcbb2
Arda K <ardakocer@live.de>
2022-10-29 00:15:57 +0200

    Fixed acknowledged interaction

--- commands/version.js
+++ commands/version.js
@@ -20,7 +20,7 @@ module.exports = {
 				{ name: 'Repository', value: 'https://github.com/suptower/soupify' },
 			)
 			.setTimestamp()
-			.setFooter({ text: 'Version ' + Version + ' | Updated: 28th of October, 2022' });
+			.setFooter({ text: 'Version ' + Version + ' | Updated: 29th of October, 2022' });
 		await interaction.editReply({ embeds: [InfoEmbed] });
 	},
 };
