bfdc3e43022a1b3c3de0a58f900c81057c560019
Arda K <ardakocer@live.de>
2023-01-31 10:18:37 +0100

    Added stats

---
+++ commands/stats.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { Op } = require('sequelize');
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('stats')
+		.setDescription('Display command stats'),
+	async execute(interaction, Tags) {
+		await interaction.deferReply();
+		await Tags.findAll({ where: {
+			command: {
+				[Op.not]: 'stats',
+			},
+		}	}).then(tagList => {
+			let tagString = 'Command name - Usage count\n';
+			for (const tag of tagList) {
+				tagString += `${tag.get('command')} - ${tag.get('usage_count')}\n`;
+			}
+			tagString += '\nTotal commands: ' + tagList.length;
+			tagString += '\nTotal usage: ' + tagList.reduce((acc, tag) => acc + tag.get('usage_count'), 0);
+			tagString += '\nAverage usage: ' + (tagList.reduce((acc, tag) => acc + tag.get('usage_count'), 0) / tagList.length).toFixed(2);
+			tagString += '\nStats counting since: 31st of January, 2023';
+			return interaction.editReply('```' + tagString + '```');
+		});
+	},
+};
